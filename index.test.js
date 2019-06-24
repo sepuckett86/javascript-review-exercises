b5ea86babddd0eba64374ff91d8d415e70cdac70
sepuckett86 <sepuckett86@gmail.com>
2019-06-24 11:28:50 -0700

    added space

--- index.test.js
+++ index.test.js
@@ -3,6 +3,7 @@ const createDog = require('./index');
 describe('createDog function', () => {
   it('returns a dog object', () => {
     const dog = createDog('spot', 5, '20lbs');
+    
     expect(dog).toEqual({
       name: 'spot',
       age: 5,
